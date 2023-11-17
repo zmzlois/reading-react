8581c4a57f5e33279b705966f9f5b1f8c6a8f938
Lois <loisisar@outlook.com>
2023-11-17 18:28:18 +0000

    init without graph

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <b>Reading React</b>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/zmzlois/reading-react',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/CPWTVStGZQ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/zmzlois/reading-react',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Reading React - zmzlois',
   },
 }
 
